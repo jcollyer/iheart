3da330f6ab046657f6154546b1e08b0a02722b78
jcollyer <collyerdesign@gmail.com>
2016-12-23 16:18:04 -0500

    refactor, cleanup

--- src/components/artists.js
+++ src/components/artists.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Search from './search'
+import { loadArtistSearch } from '../actions/artists'
+import request from 'superagent'
 
 class Artists extends Component {
+  search(evt){
+    var that = this
+    var string = document.getElementById("searchInput").value
+    if(string != ""){
+      const url = "http://api-3283.iheart.com/api/v1/catalog/searchAll?keywords="+string+"&queryTrack=false&queryBundle=false&queryArtist=true&queryStation=false&queryFeaturedStation=false&queryTalkShow=false&queryTalkTheme=false&queryKeyword=false&countryCode=US"
+      request
+        .get(url)
+        .end((error, response) => {
+          var artists = response.body.artists
+          this.props.loadArtistSearch(artists)
+        })
+    }
+  }
   render() {
     const { artists } = this.props
     return (
       <span>
-        <Search />
+        <Search search={(e) => this.search(e)} />
         {artists.map((artist,i)=>{
           const imgUrl = "http://iscale.iheart.com/catalog/artist/" + artist.artistId + "?ops=fit(250,0)"
           return (
@@ -23,10 +38,9 @@ class Artists extends Component {
 }
 
 const mapStateToProps = (appState) => {
-  // debugger;
   return {
     artists: appState.artists.artists
   }
 }
 
-export default connect(mapStateToProps)(Artists)
+export default connect(mapStateToProps, { loadArtistSearch })(Artists)
--- src/reducers/artists.js
+++ src/reducers/artists.js
@@ -1,10 +1,8 @@
 import { initialState } from '../api/initial-state'
 
 export default (state = initialState.artistList, action) => {
-  // debugger;
   switch(action.type){
     case "ARTIST_SEARCH":
-      // debugger;
       return Object.assign({}, state, {
         artists: action.artists
       });
